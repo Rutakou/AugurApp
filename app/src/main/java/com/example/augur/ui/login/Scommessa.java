92e6900aa87a27f8dcb125034bc6f2d32979c08b
Rutakou <45456599+Rutakou@users.noreply.github.com>
2022-02-11 23:51:38 +0100

    Aggiunta get all'endpoint che ritorna prezzo BTC

--- app/src/main/java/com/example/augur/ui/login/Scommessa.java
+++ app/src/main/java/com/example/augur/ui/login/Scommessa.java
@@ -11,10 +11,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.example.augur.R;
 import com.example.augur.databinding.ActivityLoginBinding;
 import com.example.augur.databinding.ActivityScommessaBinding;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.google.android.material.snackbar.Snackbar;
+import com.jacksonandroidnetworking.JacksonParserFactory;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
 
 public class Scommessa extends AppCompatActivity {
     private LoginViewModel loginViewModel;
@@ -25,9 +37,21 @@ public class Scommessa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityScommessaBinding.inflate(getLayoutInflater());
+        AndroidNetworking.initialize(getApplicationContext());
+
         setContentView(binding.getRoot());
 
+        // Adding an Network Interceptor for Debugging purpose :
+        OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                        .build();
+        AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+
+
+
         final Button scommettiButton = binding.nextButton;
+
 
         scommettiButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +66,24 @@ public class Scommessa extends AppCompatActivity {
                 finish();
             }
         });
+
+        System.out.println("Sto inviando la richiesta");
+        final String url = "http://10.0.2.2:3000/";
+
+        AndroidNetworking.get(url).build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("Risposta ricevuta");
+                        System.out.println(response);
+
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        System.out.println("Si e' verificato un errore");
+                        System.out.println(error);
+                    }
+                });
 
     }
 }
