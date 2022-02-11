d791dcc389f1d1265aa034d64ac460ec9644742b
Rutakou <45456599+Rutakou@users.noreply.github.com>
2022-02-11 09:31:57 +0100

    Aggiunta la schermata del risultato. Dopo aggiungerò animazioni, logout button e cambiare dinamicamente la scritta

---
+++ app/src/main/java/com/example/augur/ui/login/Risultato.java
@@ -0,0 +1,16 @@
+package com.example.augur.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.augur.R;
+
+public class Risultato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risultato);
+    }
+}
--- app/src/main/java/com/example/augur/ui/login/Scommessa.java
+++ app/src/main/java/com/example/augur/ui/login/Scommessa.java
@@ -2,7 +2,10 @@ package com.example.augur.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.augur.databinding.ActivityLoginBinding;
 import com.example.augur.databinding.ActivityScommessaBinding;
@@ -16,5 +19,15 @@ public class Scommessa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityScommessaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        final Button loginButton = binding.nextButton;
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Risultato.class));
+                finish();
+            }
+        });
     }
 }
