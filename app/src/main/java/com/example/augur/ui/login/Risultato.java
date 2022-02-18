784719a88c71ce21d51900d0d3a4be5a9a003b6c
SmerryDonna <smerry1998@gmail.com>
2022-02-18 01:00:21 +0100

    Aggiunti valori textview in Risultato (valore scommesso e valore BTC). Effettuato confronto tra le due con relativa modifica della textview "complimenti"

--- app/src/main/java/com/example/augur/ui/login/Risultato.java
+++ app/src/main/java/com/example/augur/ui/login/Risultato.java
@@ -15,15 +15,32 @@ import com.example.augur.databinding.ActivityScommessaBinding;
 public class Risultato extends AppCompatActivity {
     private LoginViewModel loginViewModel;
     private ActivityRisultatoBinding binding;
+    private TextView c;
+    private TextView c2;
+    private TextView c3;
+    private Bundle extras;
+    private String value;
+    private String value1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityRisultatoBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_risultato);
         final Button logoutButton = binding.bottonelogout;
-
+        extras = getIntent().getExtras();
+        c = findViewById(R.id.complimenti);
+        if (extras != null) {
+            value = extras.getString("risposta");
+            value1 = extras.getString("valorefuturo");
+            //The key argument here must match that used in the other activity
+        }
+        c2 = findViewById(R.id.scommessafatta);
+        System.out.print(value1);
+        c3 = findViewById(R.id.scommessafattaora);
+        c2.setText(value1);
+        c3.setText(value);
+        determinaRisultato(value1,value);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +52,14 @@ public class Risultato extends AppCompatActivity {
             }
         });
 
+    }
 
+    public void determinaRisultato(String uno, String due){
+        if(uno.equals(due)==true){
+            c.setText("Complimenti!");
+        }else{
+            c.setText("Hai perso!");
+        }
     }
 
 }
--- app/src/main/java/com/example/augur/ui/login/Scommessa.java
+++ app/src/main/java/com/example/augur/ui/login/Scommessa.java
@@ -1,5 +1,7 @@
 package com.example.augur.ui.login;
 
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,6 +26,7 @@ import com.google.android.material.snackbar.Snackbar;
 import com.jacksonandroidnetworking.JacksonParserFactory;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import okhttp3.OkHttpClient;
@@ -32,15 +35,20 @@ public class Scommessa extends AppCompatActivity {
     private LoginViewModel loginViewModel;
     private ActivityScommessaBinding binding;
     private TextView uno;
+    private String risposta;
+    private double rispostaDouble;
+    private String valorefuturo;
+    private String quantitascommessa;
+    private String indirizzoportafoglio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityScommessaBinding.inflate(getLayoutInflater());
         AndroidNetworking.initialize(getApplicationContext());
-
         setContentView(binding.getRoot());
-
+        Intent i = new Intent(Scommessa.this, Risultato.class);
+        Bundle extras = new Bundle();
         // Adding an Network Interceptor for Debugging purpose :
         OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
                 .addNetworkInterceptor(new StethoInterceptor())
@@ -48,24 +56,7 @@ public class Scommessa extends AppCompatActivity {
         AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
         AndroidNetworking.setParserFactory(new JacksonParserFactory());
 
-
-
         final Button scommettiButton = binding.nextButton;
-
-
-        scommettiButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String valorefuturo = binding.vf.getText().toString();
-                String quantitascommessa = binding.qs.getText().toString();
-                String indirizzoportafoglio = binding.ipp.getText().toString();
-                DatiScommessa recap = new DatiScommessa(valorefuturo,quantitascommessa,indirizzoportafoglio);
-
-                startActivity(new Intent(getApplicationContext(), Risultato.class));
-                overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
-                finish();
-            }
-        });
 
         System.out.println("Sto inviando la richiesta");
         final String url = "http://10.0.2.2:3000/";
@@ -75,7 +66,15 @@ public class Scommessa extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         System.out.println("Risposta ricevuta");
-                        System.out.println(response);
+                        try {
+                            risposta = response.get("btc_price").toString();
+                            rispostaDouble = Double.parseDouble(risposta);
+                            rispostaDouble = Math.round(rispostaDouble*100.0)/100.0;
+                            risposta = String.valueOf(rispostaDouble);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        //System.out.println(response);
 
                     }
                     @Override
@@ -85,5 +84,22 @@ public class Scommessa extends AppCompatActivity {
                     }
                 });
 
+        scommettiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorefuturo = binding.vf.getText().toString();
+                quantitascommessa = binding.qs.getText().toString();
+                indirizzoportafoglio = binding.ipp.getText().toString();
+                extras.putString("risposta", risposta);
+                extras.putString("valorefuturo", valorefuturo);
+                i.putExtras(extras);
+                startActivity(i);
+                overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
+                finish();
+            }
+        });
+
+
     }
+
 }
